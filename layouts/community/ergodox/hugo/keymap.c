fa8a33ddfe857e539b7ecc12bb2c7ea2374ae5da
Hugo Hörnquist <hugo@hornquist.se>
2019-07-20 01:27:54 +0200

    Work on Hugo.

--- layouts/community/ergodox/hugo/keymap.c
+++ layouts/community/ergodox/hugo/keymap.c
@@ -53,19 +53,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(PLVR/*SYMB*/),
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_LALT,       KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC ,KC_LGUI,KC_END,
+        KC_LALT,        KC_QUOT,      KC_LEFT,KC_RGHT,KC_LALT,
+
+                                                       KC_HOME, KC_END,
+                                                               KC_PGUP,
+                                               KC_SPC ,KC_LGUI,KC_PGDN,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSPC,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   KC_DOWN,KC_UP,  KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        KC_RALT,
-             KC_PGUP,
-             KC_PGDN,LT(OWN, KC_ENT), KC_RALT
+             KC_LEFT, KC_RIGHT,
+             KC_UP,
+             KC_DOWN,LT(OWN, KC_ENT), KC_RALT
     ),
 /* Keymap 1: Symbol Layer
  *
